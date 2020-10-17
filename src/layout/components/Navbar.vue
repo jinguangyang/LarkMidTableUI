32ee2641b73a862d2de768a6d62ac697cb14a7a1
mrzhangxuliang <1044890297@qq.com>
2020-10-17 11:52:33 +0800

    ui增加hana选项,增加元数据管理和数据治理检测(前端未写)

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -20,7 +20,7 @@
 
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
-          <img src="../../../public/avatar.jpg" class="user-avatar">
+          <img src="../../../public/logo.png" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
--- src/views/datax/json-build/writer/RDBMSWriter.vue
+++ src/views/datax/json-build/writer/RDBMSWriter.vue
@@ -16,7 +16,7 @@
           />
         </el-select>
       </el-form-item>
-      <el-form-item v-show="dataSource==='postgresql' || dataSource==='oracle' ||dataSource==='sqlserver'" label="Schema：" prop="tableSchema">
+      <el-form-item v-show="dataSource==='postgresql' || dataSource==='oracle' ||dataSource==='sqlserver' || dataSource==='hana'" label="Schema：" prop="tableSchema">
         <el-select v-model="writerForm.tableSchema" filterable style="width: 300px" @change="schemaChange">
           <el-option
             v-for="item in schemaList"
@@ -66,167 +66,167 @@
 </template>
 
 <script>
-import * as dsQueryApi from '@/api/metadata-query'
-import { list as jdbcDsList } from '@/api/datax-jdbcDatasource'
-import Bus from '../busWriter'
-export default {
-  name: 'RDBMSWriter',
-  data() {
-    return {
-      jdbcDsQuery: {
-        current: 1,
-        size: 200,
-        ascs: 'datasource_name'
-      },
-      wDsList: [],
-      schemaList: [],
-      fromTableName: '',
-      fromColumnList: [],
-      wTbList: [],
-      dataSource: '',
-      createTableName: '',
-      writerForm: {
-        datasourceId: undefined,
-        tableName: '',
-        columns: [],
-        checkAll: false,
-        isIndeterminate: true,
-        preSql: '',
-        postSql: '',
-        ifCreateTable: false,
-        tableSchema: ''
-      },
-      readerForm: this.getReaderData(),
-      rules: {
-        datasourceId: [{ required: true, message: 'this is required', trigger: 'change' }],
-        tableName: [{ required: true, message: 'this is required', trigger: 'change' }],
-        tableSchema: [{ required: true, message: 'this is required', trigger: 'change' }]
-      }
-    }
-  },
-  watch: {
-    'writerForm.datasourceId': function(oldVal, newVal) {
-      if (this.dataSource === 'postgresql' || this.dataSource === 'oracle' || this.dataSource === 'sqlserver') {
-        this.getSchema()
-      } else {
-        this.getTables('rdbmsWriter')
-      }
-    }
-  },
-  created() {
-    this.getJdbcDs()
-  },
-  methods: {
-    // 获取可用数据源
-    getJdbcDs() {
-      this.loading = true
-      jdbcDsList(this.jdbcDsQuery).then(response => {
-        const { records } = response
-        this.wDsList = records
-        this.loading = false
-      })
-    },
-    // 获取表名
-    getTables(type) {
-      if (type === 'rdbmsWriter') {
-        let obj = {}
-        if (this.dataSource === 'postgresql' || this.dataSource === 'oracle' || this.dataSource === 'sqlserver') {
-          obj = {
-            datasourceId: this.writerForm.datasourceId,
-            tableSchema: this.writerForm.tableSchema
-          }
-        } else {
-          obj = {
-            datasourceId: this.writerForm.datasourceId
-          }
+    import * as dsQueryApi from '@/api/metadata-query'
+    import { list as jdbcDsList } from '@/api/datax-jdbcDatasource'
+    import Bus from '../busWriter'
+    export default {
+        name: 'RDBMSWriter',
+        data() {
+            return {
+                jdbcDsQuery: {
+                    current: 1,
+                    size: 200,
+                    ascs: 'datasource_name'
+                },
+                wDsList: [],
+                schemaList: [],
+                fromTableName: '',
+                fromColumnList: [],
+                wTbList: [],
+                dataSource: '',
+                createTableName: '',
+                writerForm: {
+                    datasourceId: undefined,
+                    tableName: '',
+                    columns: [],
+                    checkAll: false,
+                    isIndeterminate: true,
+                    preSql: '',
+                    postSql: '',
+                    ifCreateTable: false,
+                    tableSchema: ''
+                },
+                readerForm: this.getReaderData(),
+                rules: {
+                    datasourceId: [{ required: true, message: 'this is required', trigger: 'change' }],
+                    tableName: [{ required: true, message: 'this is required', trigger: 'change' }],
+                    tableSchema: [{ required: true, message: 'this is required', trigger: 'change' }]
+                }
+            }
+        },
+        watch: {
+            'writerForm.datasourceId': function(oldVal, newVal) {
+                if (this.dataSource === 'postgresql' || this.dataSource === 'oracle' || this.dataSource === 'sqlserver' || this.dataSource === 'hana') {
+                    this.getSchema()
+                } else {
+                    this.getTables('rdbmsWriter')
+                }
+            }
+        },
+        created() {
+            this.getJdbcDs()
+        },
+        methods: {
+            // 获取可用数据源
+            getJdbcDs() {
+                this.loading = true
+                jdbcDsList(this.jdbcDsQuery).then(response => {
+                    const { records } = response
+                    this.wDsList = records
+                    this.loading = false
+                })
+            },
+            // 获取表名
+            getTables(type) {
+                if (type === 'rdbmsWriter') {
+                    let obj = {}
+                    if (this.dataSource === 'postgresql' || this.dataSource === 'oracle' || this.dataSource === 'sqlserver' || this.dataSource === 'hana') {
+                        obj = {
+                            datasourceId: this.writerForm.datasourceId,
+                            tableSchema: this.writerForm.tableSchema
+                        }
+                    } else {
+                        obj = {
+                            datasourceId: this.writerForm.datasourceId
+                        }
+                    }
+                    // 组装
+                    dsQueryApi.getTables(obj).then(response => {
+                        this.wTbList = response
+                    })
+                }
+            },
+            getSchema() {
+                const obj = {
+                    datasourceId: this.writerForm.datasourceId
+                }
+                dsQueryApi.getTableSchema(obj).then(response => {
+                    this.schemaList = response
+                })
+            },
+            // schema 切换
+            schemaChange(e) {
+                this.writerForm.tableSchema = e
+                // 获取可用表
+                this.getTables('rdbmsWriter')
+            },
+            wDsChange(e) {
+                // 清空
+                this.writerForm.tableName = ''
+                this.writerForm.datasourceId = e
+                this.wDsList.find((item) => {
+                    if (item.id === e) {
+                        this.dataSource = item.datasource
+                    }
+                })
+                Bus.dataSourceId = e
+                this.$emit('selectDataSource', this.dataSource)
+            },
+            // 获取表字段
+            getColumns() {
+                const obj = {
+                    datasourceId: this.writerForm.datasourceId,
+                    tableName: this.writerForm.tableName
+                }
+                dsQueryApi.getColumns(obj).then(response => {
+                    this.fromColumnList = response
+                    this.writerForm.columns = response
+                    this.writerForm.checkAll = true
+                    this.writerForm.isIndeterminate = false
+                })
+            },
+            // 表切换
+            wTbChange(t) {
+                this.writerForm.tableName = t
+                this.fromColumnList = []
+                this.writerForm.columns = []
+                this.getColumns('writer')
+            },
+            wHandleCheckAllChange(val) {
+                this.writerForm.columns = val ? this.fromColumnList : []
+                this.writerForm.isIndeterminate = false
+            },
+            wHandleCheckedChange(value) {
+                const checkedCount = value.length
+                this.writerForm.checkAll = checkedCount === this.fromColumnList.length
+                this.writerForm.isIndeterminate = checkedCount > 0 && checkedCount < this.fromColumnList.length
+            },
+            getData() {
+                if (Bus.dataSourceId) {
+                    this.writerForm.datasourceId = Bus.dataSourceId
+                }
+                return this.writerForm
+            },
+            getReaderData() {
+                return this.$parent.getReaderData()
+            },
+            getTableName() {
+                return this.fromTableName
+            },
+            createTable() {
+                const obj = {
+                    datasourceId: this.writerForm.datasourceId,
+                    tableName: this.createTableName
+                }
+                dsQueryApi.createTable(obj).then(response => {
+                    this.$notify({
+                        title: 'Success',
+                        message: 'Create Table Successfully',
+                        type: 'success',
+                        duration: 2000
+                    })
+                }).catch(() => console.log('promise catch err'))
+            }
         }
-        // 组装
-        dsQueryApi.getTables(obj).then(response => {
-          this.wTbList = response
-        })
-      }
-    },
-    getSchema() {
-      const obj = {
-        datasourceId: this.writerForm.datasourceId
-      }
-      dsQueryApi.getTableSchema(obj).then(response => {
-        this.schemaList = response
-      })
-    },
-    // schema 切换
-    schemaChange(e) {
-      this.writerForm.tableSchema = e
-      // 获取可用表
-      this.getTables('rdbmsWriter')
-    },
-    wDsChange(e) {
-      // 清空
-      this.writerForm.tableName = ''
-      this.writerForm.datasourceId = e
-      this.wDsList.find((item) => {
-        if (item.id === e) {
-          this.dataSource = item.datasource
-        }
-      })
-      Bus.dataSourceId = e
-      this.$emit('selectDataSource', this.dataSource)
-    },
-    // 获取表字段
-    getColumns() {
-      const obj = {
-        datasourceId: this.writerForm.datasourceId,
-        tableName: this.writerForm.tableName
-      }
-      dsQueryApi.getColumns(obj).then(response => {
-        this.fromColumnList = response
-        this.writerForm.columns = response
-        this.writerForm.checkAll = true
-        this.writerForm.isIndeterminate = false
-      })
-    },
-    // 表切换
-    wTbChange(t) {
-      this.writerForm.tableName = t
-      this.fromColumnList = []
-      this.writerForm.columns = []
-      this.getColumns('writer')
-    },
-    wHandleCheckAllChange(val) {
-      this.writerForm.columns = val ? this.fromColumnList : []
-      this.writerForm.isIndeterminate = false
-    },
-    wHandleCheckedChange(value) {
-      const checkedCount = value.length
-      this.writerForm.checkAll = checkedCount === this.fromColumnList.length
-      this.writerForm.isIndeterminate = checkedCount > 0 && checkedCount < this.fromColumnList.length
-    },
-    getData() {
-      if (Bus.dataSourceId) {
-        this.writerForm.datasourceId = Bus.dataSourceId
-      }
-      return this.writerForm
-    },
-    getReaderData() {
-      return this.$parent.getReaderData()
-    },
-    getTableName() {
-      return this.fromTableName
-    },
-    createTable() {
-      const obj = {
-        datasourceId: this.writerForm.datasourceId,
-        tableName: this.createTableName
-      }
-      dsQueryApi.createTable(obj).then(response => {
-        this.$notify({
-          title: 'Success',
-          message: 'Create Table Successfully',
-          type: 'success',
-          duration: 2000
-        })
-      }).catch(() => console.log('promise catch err'))
     }
-  }
-}
 </script>
